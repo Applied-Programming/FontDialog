91b538c442c67bca86558863594832d138d1e67c
Applied-Programming <aniruddhastapas@gmail.com>
2017-01-08 03:55:06 +0530

    Commit

---
+++ Fontdialog/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Fontdialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer component = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="d">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool d)
+        {
+            if (d && (component != null))
+            {
+                component.Dispose();
+            }
+            base.Dispose(d);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 69);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 181);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select Font";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Fontdialog/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Fontdialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FontDialog fd_obj = new FontDialog();
+            fd_obj.MinSize = 10;
+            fd_obj.MaxSize = 40;
+            fd_obj.ShowColor = true;
+            fd_obj.ShowHelp = true;
+            fd_obj.HelpRequest += new EventHandler(fd_obj_HelpRequest);
+            if (fd_obj.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Font = fd_obj.Font;
+                textBox1.ForeColor = fd_obj.Color;
+
+            }
+        }
+
+        void fd_obj_HelpRequest(object sender, EventArgs e)
+        {
+            MessageBox.Show("Choose the font and the color of the text");
+        }
+    }
+}
